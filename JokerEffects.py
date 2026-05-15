98c453311d3ee45148ab46ae2823e142c690ead4
Ausipainter <kjone795@evit.edu>
2026-05-15 13:17:54 -0700

    Lost King Joker

--- JokerEffects.py
+++ JokerEffects.py
@@ -6,11 +6,17 @@ last_hand_counter = 0
 FrogCounter = 0
 YinYang_Active = False
 poolMoney = 0
+BunsKingScale = {
+    'TimesMult' : 0,
+    'AddMult' : 0,
+    'AddChips' : 0,
+}
 skipMult = 1
 exponentJoker = 1
 luck = 1
+bunsking = False
 def reset_joker_variables():
-    global wetFloorValue, last_hand, last_hand_counter, FrogCounter, YinYang_Active, poolMoney, skipMult, exponentJoker
+    global wetFloorValue, last_hand, last_hand_counter, FrogCounter, YinYang_Active, poolMoney, skipMult, exponentJoker,bunsking,luck,BunsKingScale
     wetFloorValue = 0
     last_hand = 0
     last_hand_counter = 0
@@ -19,6 +25,13 @@ def reset_joker_variables():
     poolMoney = 0
     skipMult = 1
     exponentJoker = 1
+    luck = 1
+    bunsking = False
+    BunsKingScale['TimesMult'] = 0
+    BunsKingScale['AddMult'] = 0
+    BunsKingScale['AddChips'] = 0
+
+
 
 class JokerEffectsManager:
     def __init__(self):
@@ -347,9 +360,11 @@ def PTSD_effect(context):
         last_hand_counter = 0
         
     else:
-        
-        last_hand_counter += 0.1
-        context["mult"] = context.get('mult', 0) * (1 + last_hand_counter)
+        if bunsking:
+            BunsKingScale['AddMult'] += 0.1
+        else:
+            last_hand_counter += 0.1
+            context["mult"] = context.get('mult', 0) * (1 + last_hand_counter)
     context.setdefault('triggered_jokers', []).append('PTSD Joker')
     return context
 
@@ -360,7 +375,11 @@ def WetFloor_effect(context):
     if has_numbered:
         wetFloorValue = 0
     else:
-        wetFloorValue += 1
+        if bunsking:
+            BunsKingScale['AddMult'] += 1
+        else:
+            wetFloorValue += 1
+        
     context.setdefault('triggered_jokers', []).append('Wet Floor Joker')
     context['mult'] = context.get('mult', 0) + wetFloorValue
     return context
@@ -397,7 +416,10 @@ def DeadFrog_effect(context):
     most_hand = context.get("most_played", 0)
     hand = context.get("hand_type", 0)
     if hand != most_hand:
-        FrogCounter += 1
+        if bunsking:
+            BunsKingScale['AddChips'] += 20
+        else:
+            FrogCounter += 1
     
     context["chips"] = context.get('chips', 0) + (20 * FrogCounter)
     context.setdefault('triggered_jokers', []).append('Dead Frog')
@@ -429,7 +451,16 @@ def Ransomware_effect(context):
     return
 
 def LostKing_effect(context):
-    return
+    mult = context.get('mult', 0)
+    chips = context.get('chips', 0)
+    mult += BunsKingScale['TimesMult'] * mult
+    mult += BunsKingScale['AddMult']
+    chips += BunsKingScale['AddChips']
+    context['mult'] = mult
+    context['chips'] = chips
+    context.setdefault('triggered_jokers', []).append('Lost King')
+
+    return context
 
 def OopyGoopy_effect(context):
     jokers = context.get('active_jokers')
@@ -659,7 +690,7 @@ JOKER_REGISTRY = {
     },
     'Lost King': {
         'events': [('on_hand_played', LostKing_effect)],
-        'description': 'Steals the scaling from all scaling jokers',
+        'description': 'Steals the scaling from all scaling jokers{break}[grey]Currently[/grey][red]x{value} Mult, +{value2} Mult, +{value3}[/red] [blue]Chips[/blue]',
         'Oopy Goopy': True
     },
 }
